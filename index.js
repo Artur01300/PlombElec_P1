2a68f035d9f001d5de1d73a17fc7ac2899f7c865
Artur01300 <artur.karapetyan@outlook.fr>
2022-05-09 16:16:23 +0200

    l'application marche, reste à amélieurer

--- index.js
+++ index.js
@@ -3,15 +3,28 @@ const form = document.getElementById('form');
 const longueur = document.getElementById('longueur');
 const largeur = document.getElementById('largeur');
 const hauteur = document.getElementById('hauteur');
-const semiEnterre = document.getElementById('semiEnterre');
-const horsSol = document.getElementById('horsSol');
 
-
- 
-// console.log(hauteur, "sdfsf")
 
 form.addEventListener('submit', (e)=>{
     e.preventDefault()
-    console.log(longueur.value, largeur.value, hauteur.value, semiEnterre.value, horsSol.value,)
+
+    const radioButtons = document.querySelectorAll('input[name="geographie"]');
+    let metrCube = longueur.value * largeur.value * hauteur.value;
+    let selectegeographie;
+    for(const radioButton of radioButtons){
+        if(radioButton.checked){
+            selectegeographie = radioButton.value;
+            break;
+        }
+    }
+    if(selectegeographie == "option1"){
+        console.log(metrCube + (metrCube * 0.20))
+
+    }
+
+    else if(selectegeographie == "option2"){
+        console.log(metrCube + (metrCube * 0.25));
+    }
+
 })
 
