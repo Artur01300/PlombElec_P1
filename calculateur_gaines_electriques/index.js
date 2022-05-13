358a63e56a614c0eb0e5b496e16a58dd0de34643
Artur01300 <artur.karapetyan@outlook.fr>
2022-05-13 10:42:25 +0200

    Code html est testé par validator w3. Séction 2.5 términé

--- calculateur_gaines_electriques/index.js
+++ calculateur_gaines_electriques/index.js
@@ -94,23 +94,22 @@ formGaines.addEventListener('submit', (e)=>{
         }
     }
 
-    let covtRadFilesInt = parseFloat(valuesRadioFils)
-    let covtRadSectionsInt = parseFloat(valuesRadioSections)
+    let covtRadFilesInt = parseFloat(valuesRadioFils);
+    let covtRadSectionsInt = parseFloat(valuesRadioSections);
     let additionInt = covtRadFilesInt * covtRadSectionsInt;
+
+    // console.log(additionInt.toFixed(2), "99")
+
     const poursent16 = 16*0.33 //5.28
     const poursent20 = 20*0.33 //6.6
     const poursent25 = 25*0.33 //8.25
+    const poursent32 = 32*0.33 //10.89
+    const poursent40 = 40*0.33 //13.2
 
     let url;
     let gaines;
 
-    // let gaine20 = 20;
-    // let gaine25 = 25;
-    // let gaine32 = 32;
-    // let gaine40 = 40;
-
-
-    //Pour Séction de 1,5 et une gaine de 16
+    //Pour Séction de 1,5 
     if(valuesRadioFils === "1" && valuesRadioSections === "1.38" && additionInt <= poursent16){
         url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-16-100-courant-10041140">Voir les produits</a>`
         gaines = '16';
@@ -125,19 +124,44 @@ formGaines.addEventListener('submit', (e)=>{
         gaines = '16';
         // </>Pour Séction de 1,5 et une gaine de 16
     }
-    // Pour Séction de 1,5 et une gaine de 20
+
     if(valuesRadioFils === "4" && valuesRadioSections === "1.38" && additionInt <= poursent20){
         url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-20-100-courant-10041540">Voir les produits</a>`
         gaines = '20';
     }
-    //</> Pour Séction de 1,5 et une gaine de 20
 
-    // Pour Séction de 1,5 et une gaine de 25
     if(valuesRadioFils === "5" && valuesRadioSections === "1.38" && additionInt <= poursent25){
         url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-25-100-courant-10041940">Voir les produits</a>`
         gaines = '25';
     }
-    // </>Pour Séction de 1,5 et une gaine de 25
+    // </>Pour Séction de 1,5
+
+    //Pour Séction de 2,5
+    if(valuesRadioFils === "1" && valuesRadioSections === "1.78" && additionInt <= poursent16){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-16-100-courant-10041140">Voir les produits</a>`
+        gaines = '16';
+    }
+    if(valuesRadioFils === "2" && valuesRadioSections === "1.78" && additionInt <= poursent16){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-16-100-courant-10041140">Voir les produits</a>`
+        gaines = '16';
+    }
+    if(valuesRadioFils === "3" && valuesRadioSections === "1.78" && additionInt <= poursent20){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-20-100-courant-10041540">Voir les produits</a>`
+        gaines = '20';
+    }
+    if(valuesRadioFils === "4" && valuesRadioSections === "1.78" && additionInt <= poursent25){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-25-100-courant-10041940">Voir les produits</a>`
+        gaines = '25';
+    }
+    if(valuesRadioFils === "5" && valuesRadioSections === "1.78" && additionInt <= poursent32){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-sp-gris-tag-32-50-courant-10042724">Voir les produits</a>`
+        gaines = '32';
+    }
+    //</>Pour Séction de 2,5
+
+    //Pour Séction de 6
+    
+    //</>Pour Séction de 6
 
     let newP = document.createElement('p');
     divResult.appendChild(newP);
