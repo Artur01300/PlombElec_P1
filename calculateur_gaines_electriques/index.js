f7415f2fa5d91c026621f4b6aa2ba2d9db106d6d
Artur01300 <artur.karapetyan@outlook.fr>
2022-05-13 09:39:04 +0200

    Pour Séction de 1,5 et une gaine de 16 à 25 impléménté

--- calculateur_gaines_electriques/index.js
+++ calculateur_gaines_electriques/index.js
@@ -2,6 +2,7 @@
 const selectOpts = document.getElementById('selectOpts');
 const radioSections = document.querySelectorAll('input[name="radioSections"]');
 const radioFils = document.querySelectorAll('input[name="fils"]');
+const divResult = document.getElementById('result');
 
 
 // form.addEventListener('submit', (e)=>{
@@ -69,6 +70,7 @@ const radioFils = document.querySelectorAll('input[name="fils"]');
 // Calculateur pour gaines électrique
 const formGaines = document.getElementById('formGaines');
 
+
 formGaines.addEventListener('submit', (e)=>{
     e.preventDefault();
 
@@ -95,14 +97,55 @@ formGaines.addEventListener('submit', (e)=>{
     let covtRadFilesInt = parseFloat(valuesRadioFils)
     let covtRadSectionsInt = parseFloat(valuesRadioSections)
     let additionInt = covtRadFilesInt * covtRadSectionsInt;
-    const poursent20 = 20*0.33
-    
+    const poursent16 = 16*0.33 //5.28
+    const poursent20 = 20*0.33 //6.6
+    const poursent25 = 25*0.33 //8.25
 
-    if(valuesRadioFils === "2" && valuesRadioSections === "1.38"){
+    let url;
+    let gaines;
 
-       if(additionInt <= poursent20){
-           console.log("test réusi")
-       }
+    // let gaine20 = 20;
+    // let gaine25 = 25;
+    // let gaine32 = 32;
+    // let gaine40 = 40;
+
+
+    //Pour Séction de 1,5 et une gaine de 16
+    if(valuesRadioFils === "1" && valuesRadioSections === "1.38" && additionInt <= poursent16){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-16-100-courant-10041140">Voir les produits</a>`
+        gaines = '16';
     }
+
+    if(valuesRadioFils === "2" && valuesRadioSections === "1.38" && additionInt <= poursent16){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-16-100-courant-10041140">Voir les produits</a>`
+        gaines = '16';
+    }
+    if(valuesRadioFils === "3" && valuesRadioSections === "1.38" && additionInt <= poursent16){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-16-100-courant-10041140">Voir les produits</a>`
+        gaines = '16';
+        // </>Pour Séction de 1,5 et une gaine de 16
+    }
+    // Pour Séction de 1,5 et une gaine de 20
+    if(valuesRadioFils === "4" && valuesRadioSections === "1.38" && additionInt <= poursent20){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-20-100-courant-10041540">Voir les produits</a>`
+        gaines = '20';
+    }
+    //</> Pour Séction de 1,5 et une gaine de 20
+
+    // Pour Séction de 1,5 et une gaine de 25
+    if(valuesRadioFils === "5" && valuesRadioSections === "1.38" && additionInt <= poursent25){
+        url = `<a href="https://www.plombelecdistribution.com/collections/cable-et-fil/products/gaine-electrique-galvanisee-icta-dp-gris-tag-25-100-courant-10041940">Voir les produits</a>`
+        gaines = '25';
+    }
+    // </>Pour Séction de 1,5 et une gaine de 25
+
+    let newP = document.createElement('p');
+    divResult.appendChild(newP);
+    newP.innerHTML = `
+    Il vous faut une gaine de <strong>${gaines}</strong> diamète.
+    <p>
+        ${url}
+    </p>
+  `
     
 });
