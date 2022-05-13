7e2b1c22a24f4470f309970b0853bfbb9de79a55
Artur01300 <artur.karapetyan@outlook.fr>
2022-05-13 11:12:15 +0200

    Travial terminé

--- calculateur_gaines_electriques/index.js
+++ calculateur_gaines_electriques/index.js
@@ -1,71 +1,7 @@
-
 const selectOpts = document.getElementById('selectOpts');
 const radioSections = document.querySelectorAll('input[name="radioSections"]');
 const radioFils = document.querySelectorAll('input[name="fils"]');
 const divResult = document.getElementById('result');
-
-
-// form.addEventListener('submit', (e)=>{
-    //     e.preventDefault();
-    
-    //     let metrCube = longueur.value * largeur.value * hauteur.value;
-    //     let selectegeographie;
-    //     for(const radioButton of radioButtons){
-//         if(radioButton.checked){
-//             selectegeographie = radioButton.value;
-//             break;
-//         }
-//     }
-//     let result = 0;
-
-//     else{
-//         //Sud de la France
-//         if(selectOpts.value === "Sud de la France" && selectegeographie == "option1"){
-//             result = metrCube;
-//         }
-//         else if(selectOpts.value === "Sud de la France" && selectegeographie == "option2"){
-//             result = metrCube + (metrCube * 0.20);
-//         }
-//         else if(selectOpts.value === "Sud de la France" && selectegeographie == "option3"){
-//             result = metrCube + (metrCube * 0.20);
-//         }
-
-
-//         //Centre de la France
-//         if(selectOpts.value === "Centre de la France" && selectegeographie == "option1"){
-//             result = metrCube + (metrCube * 0.25);
-//         }
-//         else if(selectOpts.value === "Centre de la France" && selectegeographie == "option2"){
-//             result = metrCube + (metrCube * 0.20) + (metrCube * 0.25)
-//         }
-//         else if(selectOpts.value === "Centre de la France" && selectegeographie == "option3"){
-//             result = metrCube + (metrCube * 0.20) + (metrCube * 0.25)
-//         }
-
-//         //Nord de la France
-//         if(selectOpts.value === "Nord de la France" && selectegeographie == "option1"){
-//             result = metrCube + (metrCube * 0.25);
-//         }
-//         else if(selectOpts.value === "Nord de la France" && selectegeographie == "option2"){
-//             result = metrCube + (metrCube * 0.20) + (metrCube * 0.25)
-//         }
-//         else if(selectOpts.value === "Nord de la France" && selectegeographie == "option3"){
-//             result = metrCube + (metrCube * 0.20) + (metrCube * 0.25)
-//         }
-//     }
-
-//     const divResult = document.getElementById('result');
-  
-//     let newP = document.createElement('p');
-//     divResult.appendChild(newP);
-//     let resultRound = Math.round(result);
-//     newP.innerHTML = `
-//     <strong>Resultat :</strong>  Il vous faut une pompe à chaleur pouvant chauffer une piscine ayant un volume de <strong> ${resultRound}</strong> m³.
-//     <p>
-//         <a href="https://www.plombelecdistribution.com/">Voir les produits</a>
-//     </p>
-//   `
-// });
 
 // Calculateur pour gaines électrique
 const formGaines = document.getElementById('formGaines');
@@ -98,13 +34,10 @@ formGaines.addEventListener('submit', (e)=>{
     let covtRadSectionsInt = parseFloat(valuesRadioSections);
     let additionInt = covtRadFilesInt * covtRadSectionsInt;
 
-    // console.log(additionInt.toFixed(2), "99")
-
     const poursent16 = 16*0.33 //5.28
     const poursent20 = 20*0.33 //6.6
     const poursent25 = 25*0.33 //8.25
     const poursent32 = 32*0.33 //10.89
-    const poursent40 = 40*0.33 //13.2
 
     let url;
     let gaines;
@@ -159,9 +92,6 @@ formGaines.addEventListener('submit', (e)=>{
     }
     //</>Pour Séction de 2,5
 
-    //Pour Séction de 6
-    
-    //</>Pour Séction de 6
 
     let newP = document.createElement('p');
     divResult.appendChild(newP);
@@ -171,5 +101,4 @@ formGaines.addEventListener('submit', (e)=>{
         ${url}
     </p>
   `
-    
 });
